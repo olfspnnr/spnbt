96d82c89e7da5ae201c89c6110b3702d52ec5f4e
Olf Spnnr <olaf@uspinner.de>
2019-05-30 17:29:36 +0200

    website anfang

--- berndsite/src/App.tsx
+++ berndsite/src/App.tsx
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./output.css";
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export interface AppProps {}
+
+export interface AppState {
+  isLoading: boolean;
+  loadingMessage: string;
+  num: number;
+}
+
+export class App extends React.Component<AppProps, AppState> {
+  initialLoadingMessage: string;
+  constructor(props: AppProps) {
+    super(props);
+    this.initialLoadingMessage = "Loading";
+    this.state = {
+      isLoading: true,
+      loadingMessage: this.initialLoadingMessage,
+      num: this.initialLoadingMessage.length
+    };
+  }
+
+  componentDidMount() {
+    let interval = setInterval(() => {
+      console.log("test");
+
+      this.setState({
+        loadingMessage: "Loading".padEnd(this.state.num, "."),
+        num:
+          this.state.num === this.initialLoadingMessage.length + 3
+            ? this.initialLoadingMessage.length
+            : this.state.num + 1
+      });
+    }, 450);
+    this.setState(
+      {
+        isLoading: false
+      },
+      () => {
+        clearInterval(interval);
+      }
+    );
+  }
+
+  render() {
+    return (
+      <div className="flex flex-1 flex-col w-full h-full">
+        {this.state.isLoading ? (
+          <div className="flex w-full h-full justify-center items-center bg-blue-100 text-3xl">
+            {this.state.loadingMessage}
+          </div>
+        ) : (
+          <div>Test</div>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
