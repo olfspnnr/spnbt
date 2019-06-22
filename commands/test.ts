53bf1f996e792947f3a2f5f63ed4a305a67e0889
Olf Spnnr <olaf@uspinner.de>
2019-06-22 15:34:56 +0200

    rld

--- commands/test.ts
+++ commands/test.ts
@@ -16,6 +16,6 @@ export const test = {
 const executeTestFunction = (message: Message, client: Client) => {
   console.log("TEST");
   let state = getState();
-  message.channel.send("test4");
+  message.channel.send("test5");
   message.deletable && message.delete(250);
 };
