eeb5a7c2a606bb5954dd6332a573823bb0715e9a
olfspnnr <olaf@uspinner.de>
2020-09-03 23:47:26 +0200

    bulkDelete fix

--- commands/bulkDelete.ts
+++ commands/bulkDelete.ts
@@ -16,18 +16,16 @@ export const bulkDelete = {
 const bulkDeleteFunc = async (message: Message, client: Client) => {
   try {
     let [userId, anzahl] = message.content.slice("!renameUser ".length).split(" ");
-    userId = userId.replace(/<@/g, "").replace(/!/g, "").replace(/>/g, "");
     let parsedAnzahl = anzahl ? parseInt(anzahl) : undefined;
     if (parsedAnzahl > 100) {
       parsedAnzahl = 100;
     }
-    console.log({ userid: userId });
     const members = await message.guild.members.fetch();
-    if (members.some((member) => member.id === userId)) {
+    if (members.some((member) => message.mentions.has(member))) {
       const msgs = await message.channel.messages.fetch({ limit: parsedAnzahl || 25 });
       writeToLogChannel(`${message.author.username} called bulkDelete`, client);
       let messagesToBeDeleted = msgs.filter(
-        (message) => message.author.id === userId && !message.pinned
+        (message) => message.mentions.has(message.author) && !message.pinned
       );
       messagesToBeDeleted.each(async (msg) => msg.deletable && (await msg.delete()));
       message.deletable && message.delete({ timeout: 500 });
